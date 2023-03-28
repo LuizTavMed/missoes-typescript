9eefd82f12b633c536d463bb10e8d5a6e7aa32ac
willyam <pimentelwillyamdev@gmail.com>
2023-03-28 10:16:14 -0300

    build: changing yarn.lock

--- src/helpers/getDatabaseType.ts
+++
@@ -1,8 +0,0 @@
-export const getDatabaseType = (database: string | undefined): "oracle" | "mysql" | "mariadb" | "postgres" | "cockroachdb" | "sqlite" | "mssql" | "sap" | "cordova" | "nativescript" | "react-native" | "sqljs" | "mongodb" | "aurora-mysql"=>{
-    if (database == "oracle" || database == "mysql" || database == "mariadb" || database == "postgres" || database == "cockroachdb" || database == "sqlite" || database == "mssql" || database == "sap" || database == "cordova" || database == "nativescript" || database == "react-native" || database == "sqljs" || database == "mongodb" || database == "aurora-mysql"){
-        return database
-    }
-    else{
-        return "oracle"
-    }
-}
