2ef1f7df347578d136cbc9650228093655d35dcf
willyam <pimentelwillyamdev@gmail.com>
2023-03-30 15:58:10 -0300

    removing useless test

--- test/unit/isDatabaseConectable.test.ts
+++
@@ -1,8 +0,0 @@
-import dataSource from '../../src/db/dataSource'
-
-test('Verifica se é possível conectar ao banco de dados da API', async () => {
-  dataSource.initialize().then(() => {
-    console.log('it has started')
-  })
-  expect(true).toBe(true)
-})
