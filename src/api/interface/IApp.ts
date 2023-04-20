ddbdf3fea7b1fb00167ad3005b0f4f620a6762ab
willyam <pimentelwillyamdev@gmail.com>
2023-04-20 16:10:14 -0300

    refactor: adding server dependency

--- src/api/interface/IApp.ts
+++ src/api/interface/IApp.ts
@@ -1,10 +1,13 @@
 import type { Express } from 'express'
+import type { Server } from 'http'
 
 interface IApp {
   readonly express: Express
   hasStarted: boolean
+  server: Server
 
   start: () => void
+  stop: () => void
 }
 
 export default IApp
