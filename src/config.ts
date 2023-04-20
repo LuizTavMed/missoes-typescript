4aa629250bc74e05f37b533034cefd25f7fe3aea
willyam <pimentelwillyamdev@gmail.com>
2023-04-20 16:10:45 -0300

    refactor: creating config file

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,17 @@
-export const config = {
-  api_port: process.env.PORT_API ?? 4000,
-  db: {
-    username: process.env.USERNAME ?? ''
+const config = {
+  api: {
+    host: process.env.HOST_API ?? 'localhost',
+    port: process.env.PORT_API ?? 4000
+  },
+  mariadb: {
+    type: 'mysql',
+    productionDatabase: process.env.PRODUCTION_DATABASE_MARIADB ?? 'intranet',
+    testDatabase: process.env.TEST_DATABASE_MARIADB ?? 'test',
+    host: process.env.HOST_DATABASE_MARIADB ?? 'localhost',
+    username: process.env.USER_DATABASE_MARIADB ?? 'root',
+    password: process.env.PASSWORD_DATABASE_MARIADB ?? 'mariadb',
+    port: process.env.PORT_DATABASE_MARIADB ?? 3306
   }
-
 }
+
+export default config
