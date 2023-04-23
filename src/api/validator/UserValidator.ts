c08996921364f75f1b7f429ba303a6e99d0b004b
willyam <willyampimenteldev@gmail.com>
2023-04-23 09:51:23 -0300

    refactor: fixing user validator

--- src/api/validator/UserValidator.ts
+++ src/api/validator/UserValidator.ts
@@ -3,15 +3,15 @@ import type IUserValidator from '../interface/IUserValidator'
 class UserValidator implements IUserValidator {
   isUserValid (login: string, password: string, permission: string): boolean {
     console.log(login, password, permission)
-    if (!this.isLoginEmpty(login) && !this.isPasswordsEmpty(password) && !this.isPermissionEmpty(permission)) {
-      return true
-    } else {
+    if (this.isLoginEmpty(login) || this.isPasswordsEmpty(password) || this.isPermissionEmpty(permission)) {
       return false
+    } else {
+      return true
     }
   }
 
   isLoginEmpty (login: string): boolean {
-    if (login == null) {
+    if (login == '' || null || undefined) {
       return true
     } else {
       return false
@@ -19,7 +19,7 @@ class UserValidator implements IUserValidator {
   }
 
   isPasswordsEmpty (passwords: string): boolean {
-    if (passwords == null) {
+    if (passwords == '' || null || undefined) {
       return true
     } else {
       return false
@@ -27,7 +27,7 @@ class UserValidator implements IUserValidator {
   }
 
   isPermissionEmpty (permission: string): boolean {
-    if (permission == null) {
+    if (permission == '' || null || undefined) {
       return true
     } else {
       return false
