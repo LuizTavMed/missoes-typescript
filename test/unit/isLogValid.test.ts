930f79a27fd152a23c8dbee4c5a5a354059f7107
willyam <pimentelwillyamdev@gmail.com>
2023-04-25 08:39:53 -0300

    refactor: changing a method name

--- test/unit/isLogValid.test.ts
+++ test/unit/isLogValid.test.ts
@@ -1,7 +1,7 @@
-import LogValidator from "../../src/api/validator/LogValidator";
+import LogValidator from '../../src/api/validator/LogValidator'
 
 test('Should verify if log is valid', () => {
   const logValidator = new LogValidator()
-  expect(logValidator.isValid('validLog')).toBe(true);
-  expect(logValidator.isValid('')).toBe(false);
+  expect(logValidator.isLogValid('validLog')).toBe(true)
+  expect(logValidator.isLogValid('')).toBe(false)
 })
