f2471768072c595fc54d91316eaf554520b3cfed
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-26 15:22:41 -0300

    feat: adding user validator

--- src/api/validator/UserValidator.ts
+++ src/api/validator/UserValidator.ts
@@ -1,16 +1,44 @@
+import type IUserRepository from '../interface/IUserRepository'
 import type IUserValidator from '../interface/IUserValidator'
 
+enum UserErrorMessage {
+  INVALID_LOGIN = 'Invalid user',
+  LOGIN_ALREADY_EXISTS = 'Login already exists',
+  INVALID_PASSWORD = 'Invalid user',
+  INVALID_TYPE = 'Invalid type',
+  INVALID_EMAIL = 'Invalid email',
+  EMAIL_ALREADY_EXISTS = 'Email already exists',
+}
+
 class UserValidator implements IUserValidator {
-  isUserValid (login: unknown, password: unknown, permission: unknown): boolean {
-    console.log(login, password, permission)
-    if (this.isLoginEmpty(login) || this.isPasswordsEmpty(password) || this.isPermissionEmpty(permission)) {
-      return false
-    } else {
+  constructor (readonly userRepository: IUserRepository) {}
+  async validate (login: string, password: string, email: string, type: string): Promise<void> {
+    if (this.isEmpty(login) || this.hasInvalidCharacters(login)) throw new Error(UserErrorMessage.INVALID_LOGIN)
+    if (this.isEmpty(password) || this.hasInvalidCharacters(password)) throw new Error(UserErrorMessage.INVALID_PASSWORD)
+    if (this.isEmpty(email)) throw new Error(UserErrorMessage.INVALID_EMAIL)
+    if (this.isEmpty(type)) throw new Error(UserErrorMessage.INVALID_TYPE)
+    if (await this.loginAlreadyExists(login)) throw new Error(UserErrorMessage.LOGIN_ALREADY_EXISTS)
+    if (await this.emailAlreadyExists(email)) throw new Error(UserErrorMessage.EMAIL_ALREADY_EXISTS)
+  }
+
+  isEmpty (value: string): boolean {
+    if (value === '' || value == null) {
       return true
+    } else {
+      return false
     }
   }
 
-  isLoginEmpty (login: unknown): boolean {
+  hasInvalidCharacters (value: string): boolean {
+    for (let i = 0; i < value.length; i++) {
+      if (value[i] === ' ' || value[i] === '!' || value[i] === '@' || value[i] === '#' || value[i] === '%') {
+        return true
+      }
+    }
+    return false
+  }
+
+  isLoginEmpty (login: string): boolean {
     if (login === '' || login === null || login === undefined) {
       return true
     } else {
@@ -18,7 +46,7 @@ class UserValidator implements IUserValidator {
     }
   }
 
-  isPasswordsEmpty (passwords: unknown): boolean {
+  isPasswordsEmpty (passwords: string): boolean {
     if (passwords === '' || passwords === null || passwords === undefined) {
       return true
     } else {
@@ -26,8 +54,24 @@ class UserValidator implements IUserValidator {
     }
   }
 
-  isPermissionEmpty (permission: unknown): boolean {
+  isPermissionEmpty (permission: string): boolean {
     if (permission === '' || permission === null || permission === undefined) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  async loginAlreadyExists (login: string): Promise<boolean> {
+    if ((await this.userRepository.getByLogin(login)) != null) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  async emailAlreadyExists (email: string): Promise<boolean> {
+    if ((await this.userRepository.getByEmail(email)) != null) {
       return true
     } else {
       return false
