f1e893aed2e59002e59d9d3f0831b6313bdb286f
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-26 15:04:41 -0300

    feat: adding log controller interface

--- src/api/interface/ILogController.ts
+++ src/api/interface/ILogController.ts
@@ -1,16 +1,14 @@
 import { type Request, type Response } from 'express'
 import type ILogValidator from './ILogValidator'
-import type ILogRepository from './ILogRepository'
+import type ILogService from './ILogService'
 
 interface ILogController {
-  readonly logRepository: ILogRepository
+  readonly logService: ILogService
   readonly logValidator: ILogValidator
 
-  create: (req: Request, res: Response) => Promise<void>
-  getAll: (res: Response) => Promise<void>
-  get: (req: Request, res: Response) => Promise<void>
-  update: (req: Request, res: Response) => Promise<void>
-  delete: (req: Request, res: Response) => Promise<void>
+  create: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>
+  getAll: (res: Response) => Promise<Response<any, Record<string, any>>>
+  get: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>
 }
 
 export default ILogController
