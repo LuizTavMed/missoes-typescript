2a44d233756640bcb838cfb29cf54c8c6a61961c
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-26 15:01:46 -0300

    feat: adding app interface

--- src/api/interface/IApp.ts
+++ src/api/interface/IApp.ts
@@ -1,11 +1,4 @@
-import type { Express } from 'express'
-import type { Server } from 'http'
-
 interface IApp {
-  readonly express: Express
-  hasStarted: boolean
-  server: Server
-
   start: () => void
   stop: () => void
 }
