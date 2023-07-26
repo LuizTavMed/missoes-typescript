9dfe4b21ff5121ed310485107d3f1927eadf5bf8
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-26 15:21:27 -0300

    feat: adding log repository

--- src/api/repository/LogRepository.ts
+++ src/api/repository/LogRepository.ts
@@ -1,58 +1,29 @@
-import getDateNow from '../../helper/getDateNow'
-
-import type { Repository, EntityTarget, ObjectLiteral, DataSource } from 'typeorm'
-
+import type IOracledbDataSource from '../interface/IOracledbDataSource'
+import type ILogRepository from '../interface/ILogRepository'
+import type IUuidGenerator from '../interface/IUuidGenerator'
+import type IDateManager from '../interface/IDateManager'
 import type ILog from '../interface/ILog'
 
-import type ILogRepository from '../interface/ILogRepository'
-
 class LogRepository implements ILogRepository {
-  readonly resource: Repository<ObjectLiteral>
+  constructor (readonly dataSource: IOracledbDataSource, readonly uuidGenerator: IUuidGenerator, readonly dateManager: IDateManager) {}
 
-  constructor (dataSource: DataSource, logEntity: EntityTarget<ObjectLiteral>) {
-    this.resource = dataSource.getRepository(logEntity)
-  }
-
-  async create (message: string): Promise<ObjectLiteral> {
-    const date = getDateNow()
-    const log: ILog = { date, message }
-    await this.resource.save(log)
+  async create (id: string, message: string, date: string): Promise<ILog> {
+    const log = { id, date, message }
+    await this.dataSource.insertLogRegistry(id, message, date)
     return log
   }
 
-  async getAll (): Promise<ObjectLiteral[] | null> {
-    const listaTodosLogs = await this.resource.find()
-    return listaTodosLogs
+  async getAll (): Promise<ILog[]> {
+    return await this.dataSource.getEveryLogRegistry()
   }
 
-  async get (id: number): Promise<ObjectLiteral | null> {
-    const log = await this.resource.findBy({ id })
-    return log[0]
-  }
-
-  async update (id: number, body: ObjectLiteral): Promise<ObjectLiteral | ObjectLiteral> {
-    const log = await this.resource.findBy({ id })
-    if (log[0] === undefined) {
-      return log[0]
+  async get (id: string): Promise<ILog | null> {
+    const logList = await this.dataSource.getLogBy('id', id)
+    const log = logList
+    if (log == null) {
+      return null
     }
-    if (body.message !== undefined) {
-      log[0].message = body.message
-    }
-    if (body.date !== undefined) {
-      log[0].date = body.date
-    }
-    await this.resource.save(log[0])
-    return log[0]
-  }
-
-  async delete (id: number): Promise<ObjectLiteral | null> {
-    const log = await this.resource.findBy({ id })
-    if (log[0] === undefined) {
-      return log[0]
-    } else {
-      await this.resource.remove(log[0])
-      return log[0]
-    }
+    return log
   }
 }
 
