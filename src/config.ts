ab148e115671b41d9ac90261fd0538c77c40f496
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-26 14:59:07 -0300

    feat: adding config

--- src/config.ts
+++ src/config.ts
@@ -14,6 +14,20 @@ const config = {
     username: process.env.USER_DATABASE_MARIADB ?? 'root',
     password: process.env.PASSWORD_DATABASE_MARIADB ?? 'mariadb',
     port: process.env.PORT_DATABASE_MARIADB ?? 3306
+  },
+  oracledb: {
+    type: 'oracle',
+    productionDatabase: process.env.PRODUCTION_DATABASE_ORACLE ?? 'intranet',
+    testDatabase: process.env.TEST_DATABASE_ORACLE ?? 'test',
+    host: process.env.HOST_DATABASE_ORACLE ?? 'localhost',
+    username: process.env.USER_DATABASE_ORACLE ?? 'sys',
+    password: process.env.PASSWORD_DATABASE_ORACLE ?? 'oracledb',
+    ssid: process.env.SSID_DATABASE_ORACLE ?? 'xe',
+    port: process.env.PORT_DATABASE_ORACLE ?? 3306,
+    connectString: process.env.CONNECTION_STRING_DATABASE_ORACLE ?? 'localhost/xe'
+  },
+  json: {
+    secret: process.env.SECRET_JSON ?? 'secret'
   }
 }
 
