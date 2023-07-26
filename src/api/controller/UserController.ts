c79576f275e80e63b620d1731b3e60fdafe36cd2
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-07-26 15:00:28 -0300

    feat: adding user controller

--- src/api/controller/UserController.ts
+++ src/api/controller/UserController.ts
@@ -1,84 +1,74 @@
 import { type Request, type Response } from 'express'
 
 import type IUserController from '../interface/IUserController'
-import type IUserRepository from '../interface/IUserRepository'
+import type IUserService from '../interface/IUserService'
 import type IUserValidator from '../interface/IUserValidator'
 
-enum UserError {
-  USER_INVALID_REQUEST = 'A requisição inserida foi considerada inválida',
-  USER_LIST_ERROR = 'Houve um erro quando tentamos buscar a lista',
-  USER_NOT_FOUND = 'Não foi possível encontrar este usuário',
-  USER_NOT_UPDATED = 'Não foi possível atualizar este usuário',
-  USER_NOT_DELETED = 'Não foi possível deletar este usuário',
+enum UserErrorMessage {
+  UNKNOWN = 'Unknown error during request',
+  NOT_FOUND = 'The user could not be found',
 }
+
 class UserController implements IUserController {
-  constructor (readonly userRepository: IUserRepository, readonly userValidator: IUserValidator) {
-    this.userRepository = userRepository
-    this.userValidator = userValidator
-  }
+  constructor (readonly userService: IUserService, readonly userValidator: IUserValidator) {}
 
-  async create (req: Request, res: Response): Promise<void> {
+  async create (req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
     try {
-      if (this.userValidator.isUserValid(req.body.login, req.body.password, req.body.permission)) {
-        const user = await this.userRepository.create(req.body.login, req.body.password, req.body.permission)
-        res.status(200).json(user)
-      } else {
-        res.status(400).send(UserError.USER_INVALID_REQUEST)
+      await this.userValidator.validate(req.body.login, req.body.password, req.body.email, req.body.type)
+      const user = await this.userService.create(req.body.login, req.body.password, req.body.email, req.body.type)
+      return res.status(200).json(user)
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(400).send(error.message)
       }
-    } catch (erro) {
-      console.error(erro)
-      res.status(400).send(UserError.USER_INVALID_REQUEST)
+      return res.status(500).send(UserErrorMessage.UNKNOWN)
     }
   }
 
-  async getAll (res: Response): Promise<void> {
+  async getAll (res: Response): Promise<Response<any, Record<string, any>>> {
     try {
-      const listaUsers = await this.userRepository.getAll()
-      res.status(200).json(listaUsers)
-    } catch (erro) {
-      console.error(erro)
-      res.status(400).send(UserError.USER_INVALID_REQUEST)
+      const userList = await this.userService.getAll()
+      return res.status(200).json(userList)
+    } catch (error) {
+      return res.status(500).send(UserErrorMessage.UNKNOWN)
     }
   }
 
-  async get (req: Request, res: Response): Promise<void> {
+  async get (req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
     try {
-      const user = await this.userRepository.get(parseInt(req.params.id))
+      const user = await this.userService.get(req.params.id)
       if (user != null) {
-        res.status(200).json(user)
+        return res.status(200).json(user)
+      }
+    } catch (err) {
+      console.error(err)
+    }
+    return res.status(500).send(UserErrorMessage.NOT_FOUND)
+  }
+
+  async update (req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
+    try {
+      const user = await this.userService.update(req.params.id, req.body)
+      if (user !== null) {
+        return res.status(200).json(user)
       } else {
-        res.status(404).send(UserError.USER_NOT_FOUND)
+        return res.status(404).send(UserErrorMessage.NOT_FOUND)
       }
     } catch (erro) {
-      res.status(400).send(UserError.USER_INVALID_REQUEST)
+      return res.status(500).send(UserErrorMessage.UNKNOWN)
     }
   }
 
-  async update (req: Request, res: Response): Promise<void> {
+  async delete (req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
     try {
-      const user = await this.userRepository.update(parseInt(req.params.id), req.body)
-      if (user !== undefined) {
-        res.status(200).json(user)
-      } else {
-        res.status(404).send(UserError.USER_NOT_UPDATED)
-      }
-    } catch (erro) {
-      console.log(erro)
-      res.status(400).send(UserError.USER_INVALID_REQUEST)
-    }
-  }
-
-  async delete (req: Request, res: Response): Promise<void> {
-    try {
-      const user = await this.userRepository.delete(parseInt(req.params.id))
+      const user = await this.userService.delete(req.params.id)
       if (user != null) {
-        res.status(200).json(user)
+        return res.status(200).json(user)
       } else {
-        res.status(404).send(UserError.USER_NOT_DELETED)
+        return res.status(404).send(UserErrorMessage.NOT_FOUND)
       }
-    } catch (erro) {
-      console.error(erro)
-      res.status(400).send(UserError.USER_INVALID_REQUEST)
+    } catch (err) {
+      return res.status(500).send(UserErrorMessage.UNKNOWN)
     }
   }
 }
