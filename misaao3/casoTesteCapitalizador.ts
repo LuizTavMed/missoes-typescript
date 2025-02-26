54b29c5027e5a29f823fa638b8bfd0648ce1c7b1
GGTI/LUIZ.FERNANDO <11830452479@sdspe.infra>
2025-02-26 14:48:00 -0300

    corrigindo erros misões 3

--- misaao3/casoTesteCapitalizador.ts
+++ misaao3/casoTesteCapitalizador.ts
@@ -1,43 +1,43 @@
-function testeCapitalizar () {
-    function capitalizadorLetras (frase: string){
-        const palavrasRestritas: string[] = ["da","de","a","e"];
-        const palavrasSeparadas = frase.toLowerCase().split(' ');
-        let fraseTratada: string = "";
+function testeCapitalizar (): void {
+  function capitalizadorLetras (frase: string): string {
+    const palavrasRestritas: string[] = ['da', 'de', 'a', 'e']
+    const palavrasSeparadas = frase.toLowerCase().split(' ')
+    let fraseTratada: string = ''
 
-        for (let i=0; i<palavrasSeparadas.length; i++){
-            if (!palavrasRestritas.includes(palavrasSeparadas[i])){
-                fraseTratada += palavrasSeparadas[i].charAt(0).toUpperCase() + palavrasSeparadas[i].slice(1) + " ";
-            } else{
-                fraseTratada += palavrasSeparadas[i] + " ";
-            }
-            return fraseTratada;
-        }
+    for (let i = 0; i < palavrasSeparadas.length; i++) {
+      if (!palavrasRestritas.includes(palavrasSeparadas[i])) {
+        fraseTratada += palavrasSeparadas[i].charAt(0).toUpperCase() + palavrasSeparadas[i].slice(1) + ' '
+      } else {
+        fraseTratada += palavrasSeparadas[i] + ' '
+      }
     }
-    
-    const fraseParaCapitalizar1: string = "unidade de sistema e aplicativos";
-    const fraseParaCapitalizar2: string = "software livre e de código aberto";
-    const fraseParaCapitalizar3: string = "UNIDADE DE SISTEMA E APLICATIVO";
+    return fraseTratada.trim()
+  }
 
-    const novaFrase1 = capitalizadorLetras(fraseParaCapitalizar1);
-    const novaFrase2 = capitalizadorLetras(fraseParaCapitalizar2);
-    const novaFrase3 = capitalizadorLetras(fraseParaCapitalizar3);
+  const fraseParaCapitalizar1: string = 'unidade de sistema e aplicativos'
+  const fraseParaCapitalizar2: string = 'software livre e de código aberto'
+  const fraseParaCapitalizar3: string = 'UNIDADE DE SISTEMA E APLICATIVO'
 
-    if (novaFrase1 === "Unidade de Sistema e Aplicativos"){
-        console.log("Teste 1: passou");
-    }else{
-        console.log("Teste 1: não passou");
-    }
+  const novaFrase1 = capitalizadorLetras(fraseParaCapitalizar1)
+  const novaFrase2 = capitalizadorLetras(fraseParaCapitalizar2)
+  const novaFrase3 = capitalizadorLetras(fraseParaCapitalizar3)
 
-    if (novaFrase2 === "Software Livre e de Código Aberto"){
-        console.log("Teste 2: passou");
-    }else{
-        console.log("Teste 2: não passou");
-    }
+  if (novaFrase1 === 'Unidade de Sistema e Aplicativos') {
+    console.log('Teste 1: passou')
+  } else {
+    console.log('Teste 1: não passou')
+  }
 
-    if (novaFrase3 === "Unidade de Sistema e Aplicativo"){
-        console.log("Teste 3: passou");
-    }else{
-        console.log("Teste 3: não passou");
-    }
+  if (novaFrase2 === 'Software Livre e de Código Aberto') {
+    console.log('Teste 2: passou')
+  } else {
+    console.log('Teste 2: não passou')
+  }
+
+  if (novaFrase3 === 'Unidade de Sistema e Aplicativo') {
+    console.log('Teste 3: passou')
+  } else {
+    console.log('Teste 3: não passou')
+  }
 }
 testeCapitalizar()
